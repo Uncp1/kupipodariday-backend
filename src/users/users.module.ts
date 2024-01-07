08167b5c00a0a817eef44d6f728e41f40a2d0187
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-01-07 19:49:18 +0300

    created modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { StudentsController } from './users.controller';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [StudentsController],
+  controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService], //?
+  exports: [UsersService],
 })
 export class UsersModule {}
