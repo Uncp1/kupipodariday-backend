630968dbb84e23ccb60665b083e6c297afaa0794
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-01-11 17:27:58 +0300

    all modules are finished

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -5,7 +5,7 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class HashService {
   async hash(password: string): Promise<string> {
-    return await bcrypt.hash(password, 10);
+    return await bcrypt.hash(password, 10); //need a fix for ConfigService
   }
 
   async verify(password: string, hash: string) {
