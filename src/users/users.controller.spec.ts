f2460362b3ac4f70f6473d911e92d9bcb636fbe7
Uncp1 <113035612+Uncp1@users.noreply.github.com>
2024-02-09 00:30:17 +0300

    need to switch

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 describe('UsersController', () => {
   let usersController: UsersController;
   let usersService: UsersService;
   const usersServiceMock = {
     findAll: jest.fn(),
-    create: jest.fn(),
+    createUser: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -20,6 +22,10 @@ describe('UsersController', () => {
     usersService = module.get<UsersService>(UsersService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('.create() should call usersService.create()', async () => {
     const mockUser = {
       username: 'testuser',
@@ -28,14 +34,50 @@ describe('UsersController', () => {
       email: 'user@test.com',
       password: 'strongpassword',
     };
-    usersService.createUser = jest.fn().mockResolvedValue(mockUser);
+    usersServiceMock.createUser.mockResolvedValue(mockUser);
 
-    await expect(usersController.create(mockUser)).resolves.toBe(mockUser);
-    expect(usersService.createUser).toHaveBeenCalledWith(mockUser);
+    const result = await usersController.create(mockUser);
+
+    expect(result).toBe(mockUser);
+    expect(usersServiceMock.createUser).toHaveBeenCalledWith(mockUser);
   });
 
   it('.get() should call UsersService.get()', () => {
     usersController.findAll();
     expect(usersService.findAll).toHaveBeenCalled();
+  });
+
+  //?????
+  it('.create() should call usersService.createUser()', async () => {
+    const createUserDto: CreateUserDto = {
+      username: 'testuser',
+      about: 'testuser',
+      avatar: 'https://i.pravatar.cc/150?img=3',
+      email: 'user@test.com',
+      password: 'strongpassword',
+    }; // Mock data for CreateUserDto
+    const expectedResult: User = {
+      username: 'testuser',
+      about: 'testuser',
+      avatar: 'https://i.pravatar.cc/150?img=3',
+      email: 'user@test.com',
+      password: 'strongpassword',
+    }; // Mock data for User
+
+    usersServiceMock.createUser.mockResolvedValue(expectedResult);
+
+    expect(await usersController.create(createUserDto)).toEqual(expectedResult);
+    expect(usersServiceMock.createUser).toHaveBeenCalledWith(createUserDto);
+  });
+
+  it('.get() should call UsersService.findAll()', async () => {
+    const expectedResult: User[] = [
+      /* ... */
+    ]; // Mock data for User array
+
+    usersServiceMock.findAll.mockResolvedValue(expectedResult);
+
+    expect(await usersController.findAll()).toEqual(expectedResult);
+    expect(usersServiceMock.findAll).toHaveBeenCalled();
   });
 });
